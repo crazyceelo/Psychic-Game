44c1cbab93701fd85f83eaff43546aeba1e1ea7c
Carlos Perez <solracias@gmail.com>
2017-04-02 13:56:22 -0700

    initialized the array and set the letter challenge

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,19 @@
-var player;
+// array of letters.
+var letters = ["a","b","c","d","e","f","g","h","i","j","h","i","j",
+                "k","l","m","n","o","p","q","r","s","t","u","v", "w", "y", "z"];
 
+// finds and sets one letter for the new challenge.
+var findLetter = Math.floor(Math.random()*letters.length);
+var setChallenge = letters[findLetter];
+
+console.log(setChallenge);
+
+// set of initial variables.
 var wins = 0;
 var losses = 0;
-var guessesLeft = 5;
-var guessesSoFar = 0;
+var guessesLeft = 10;
+var guessesSoFar = "";
+
+
+
+
