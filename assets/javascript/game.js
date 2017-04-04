8cafe83bfd6e1ae41ebb78f07aa83894a3c6ace1
caperez <caperez@blizzard.com>
2017-04-03 21:35:57 -0700

    everything now resets except for the challenge

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,10 @@ var letters = ["a","b","c","d","e","f","g","h","i","j","h","i","j",
                 "k","l","m","n","o","p","q","r","s","t","u","v", "w", "y", "z"];
 
 // finds and sets one letter from the array and sets the new challenge.
+
 var findLetter = Math.floor(Math.random()*letters.length);
 var setChallenge = letters[findLetter];
+
 
 console.log(setChallenge);
 
@@ -12,42 +14,51 @@ console.log(setChallenge);
 var win = false;
 var wins = 0;
 var losses = 0;
-var userGuess = "";
 var guessesLeft = 10;
-var guessesSoFar = userGuess;
+var guessesSoFar = [];
 
-// records the key press of user into lowercase.
+
 document.onkeyup = function()
-{
-  userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+{ 
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  if (guessesLeft < 2) 
+  if (guessesLeft == 0) 
   {
     win = false;
+    losses++;
     alert("you lose");
+    guessesLeft = 10
+    guessesSoFar = [];
   }
 
   else if (setChallenge == userGuess) 
   {
     win = true;
+    wins++;
     alert("you win");
+    guessesLeft = 10
+    guessesSoFar = [];
   }
 
-  else if (userGuess != setChallenge) 
+  else
   {
     guessesLeft = guessesLeft - 1;
+    guessesSoFar.push(userGuess);
+    guessesSoFar.join(",");
   }
+
+  var html = "<h1>The Psychic Game</h1>" +
+  "<p>wins: " + wins + "</p>" + 
+  "<p>losses: " + losses + "</p>" +
+  "<p>Guesses left: " + guessesLeft + "</p>" +
+  "<p>Your guesses so far: " + guessesSoFar + "</p>";
+
+
+  document.querySelector("#game").innerHTML = html;
 }
 
 // holds all text we want displayed once user starts pressing keys
-var html = "<h1>The Psychic Game</h1>" +
-"<p>wins: " + wins + "</p>" + 
-"<p>losses: " + losses + "</p>" +
-"<p>Guesses left: " + guessesLeft + "</p>" +
-"<p>Your guesses so far: " + guessesSoFar + "</p>";
 
-
-document.querySelector("#game").innerHTML = html;
 
 
 
